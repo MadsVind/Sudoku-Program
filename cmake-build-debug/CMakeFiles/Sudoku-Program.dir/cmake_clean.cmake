f1278dff7f270f21e7750773adef1615e0c249bd
Mads Vind Knudsen <madsvind7@gmail.com>
2022-09-12 16:00:57 +0200

    The basic board solver is working in this version, so does the board loader from file. An attempt to create files has been made...

---
+++ cmake-build-debug/CMakeFiles/Sudoku-Program.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Sudoku-Program.dir/main.c.obj"
+  "CMakeFiles/Sudoku-Program.dir/main.c.obj.d"
+  "Sudoku-Program.exe"
+  "Sudoku-Program.exe.manifest"
+  "Sudoku-Program.pdb"
+  "libSudoku-Program.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Sudoku-Program.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/Sudoku-Program.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Sudoku-Program.
+# This may be replaced when dependencies are built.
