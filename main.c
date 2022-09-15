c9120c211ffcad89ebad47fe7710f57def94f42b
Mads Vind Knudsen <madsvind7@gmail.com>
2022-09-15 20:44:49 +0200

    Made the user input to sudoku board and file work

--- main.c
+++ main.c
@@ -10,30 +10,30 @@ int *** makeBoardFromFile(char* file);
 
 int *** uiTable();
 
-void createSudokuFile(int*** pboard);
+void createSudokuFile(int*** pBoard);
 
-int *** solveTable(int*** pboard);
+int *** solveTable(int*** pBoard);
 
-int * possibilities(int*** pboard, int i, int j);
+int * possibilities(int*** pBoard, int i, int j);
 
-int findIndexOf(int numb, int* posiArray);
+int findIndexOf(int numb, int* posArray);
 
-void printTable(int*** pboard);
+void printTable(int*** pBoard);
 
-void free3DpArray(int ***pboard);
+void free3DpArray(int ***pBoard);
 
 int main() {
-    printf("Write the name of the file with the Sudoku board (max of 20 chars)\n =>");
-    char pfileName[20];
+    /*printf("Write the name of the file with the Sudoku board (max of 20 chars)\n =>");
+    char pFileName[20];
 
     scanf("%s", &pfileName);
 
-    int *** standardBoard = makeBoardFromFile(pfileName);
-
-    /*
-    int *** standardBoard = uiTable(standardBoard);
-    createSudokuFile(standardBoard);
+    int *** standardBoard = makeBoardFromFile(pFileName);
     */
+
+    int *** standardBoard = uiTable();
+    createSudokuFile(standardBoard);
+
 
 
     printTable(solveTable(standardBoard));
@@ -42,32 +42,35 @@ int main() {
 }
 
 int *** make3dArray() {
-    int*** pboard = malloc(sizeof(int**) * BOARD_SIZE);
+    int*** pBoard = malloc(sizeof(int**) * BOARD_SIZE);
     for (int i = 0; i < BOARD_SIZE; i++) {
-        pboard[i] = malloc(sizeof(int*) * BOARD_SIZE);
+        pBoard[i] = malloc(sizeof(int*) * BOARD_SIZE);
         for (int j = 0; j < BOARD_SIZE; j++) {
-            pboard[i][j] = malloc(sizeof(int) * MULIGE_INTS);
+            pBoard[i][j] = malloc(sizeof(int) * MULIGE_INTS);
         }
     }
-    return pboard;
+    return pBoard;
 }
-
+/*
+ * Reads from file with a sudoku board to make a 4D pointer array with the sudoku boards numbers in the 0 position in
+ * fourth layer by going through 2 for loops.
+ * */
 int *** makeBoardFromFile(char* file) {
-    int*** pboard = make3dArray();                                   //makes pointer array in the size of a sudoku board
-    FILE * fpointer;                                                //makes a pointer to a file
+    int*** pBoard = make3dArray();                                  //makes pointer array in the size of a sudoku board
+    FILE * pFile;                                                   //makes a pointer to a file
     printf("file with the name %s", file);
-    fpointer = fopen(file, "r");                       //locates the file for for above pointer and says its for reading
-    char c;                                                         //c represents one char in the file
+    pFile = fopen(file, "r");                          //locates the file for the above pointer and says it's for reading
+    signed char c;                                                  //c represents one char in the file
 
-    if (fpointer == NULL) {                                         //check if there is anything at the address of the pointer (the file)
+    if (pFile == NULL) {                                            //check if there is anything at the address of the pointer (the file)
         printf("There was no file with the name %s", file);   // if it is not the case it informs the user
-        return pboard;                                               // and then return to the call
+        return pBoard;                                              // and then return to the call
     }
     else {                                                          //If it's not the case it starts to take what is in the-
         int i = 0, j = 0;                                           //file and tries to put it in the pointer array of a board
-        while ((c = fgetc(fpointer)) != EOF) {                  //gets a char and put it in c from the file as long as the end hasn't been reached
+        while ((c = fgetc(pFile)) != EOF) {                     //gets a char and put it in c from the file as long as the end hasn't been reached
             if (c != ' ' && c != '\n') {                            //makes sure c is actually a character
-                pboard[i][j][0] = c - '0';                           //converts char to int, something about ascii and  negating by 0
+                pBoard[i][j][0] = c - '0';                          //converts char to int, something about ascii and  negating by 0
                 i++;
                 if (i == 9) {                                       //next line is reached on the board
                     i = 0;
@@ -79,33 +82,37 @@ int *** makeBoardFromFile(char* file) {
             }
         }
     }
-    fclose(fpointer);
-    return pboard;
+    fclose(pFile);
+    return pBoard;
 }
+
+/*
+ * Makes sudoku board in 4D pointer array from user input by 2 for loops.
+ */
 int *** uiTable() {
-    int *** pboard = make3dArray();
+    int *** pBoard = make3dArray();
     for (int i = 0; i < BOARD_SIZE; i++) {
         for (int j = 0; j < BOARD_SIZE; j++) {
-            int ui;
+            int ui = 0;
             printf("%d, %d: ", i, j);
             scanf( "%d", &ui);
-            if (ui > 0 && ui < 10) {
-                pboard[i][j][0] = ui;
-            }
-            else {
-                pboard[i][j][0] = 0;
+            pBoard[i][j][0] = ui;
+            while (ui < 0 || ui >= 10) {
+                printf("Error 400, try again: %d, %d: ", i, j);
+                scanf( "%d", &ui);
+                pBoard[i][j][0] = ui;
             }
         }
     }
-    return pboard;
+    return pBoard;
 }
 
-void createSudokuFile(int*** pboard) {
+void createSudokuFile(int*** pBoard) {
     char fileName[20];
     printf("Name the file for the Sudoku board (max 20 chars) \n => ");
-    scanf("s%", &fileName);
+    scanf("%s", &fileName);
     FILE * pFile;
-    pFile = fopen(fileName, "w");
+    pFile = fopen(fileName, "w+");
 
     char sudokuString[400];
 
@@ -113,7 +120,7 @@ void createSudokuFile(int*** pboard) {
 
     for (int x = 0; x < BOARD_SIZE; ++x) {
         for (int y = 0; y < BOARD_SIZE; ++y) {
-            sudokuString[i] = pboard[x][y][0] + '0';
+            sudokuString[i] = pBoard[x][y][0] + '0';
             i++;
             sudokuString[i] = ' ';
             i++;
@@ -121,14 +128,15 @@ void createSudokuFile(int*** pboard) {
         sudokuString[i] = '\n';
         i++;
     }
+    fputs(sudokuString, pFile);
     fclose(pFile);
 }
 
-void printTable(int*** pboard) {
+void printTable(int*** pBoard) {
     printf("\n");
     for (int i = 0; i < BOARD_SIZE; i++) {
         for (int j = 0; j < BOARD_SIZE; j++) {
-            printf("%d ",pboard[i][j][0]);
+            printf("%d ",pBoard[i][j][0]);
             if (j == BOARD_SIZE-1) {
                 printf("\n");
             }
@@ -136,45 +144,46 @@ void printTable(int*** pboard) {
     }
 }
 
-int *** solveTable(int*** pboard) {
-    printTable(pboard);
+int *** solveTable(int*** pBoard) {
+    printTable(pBoard);
 
-    int zeroCounter;
+    int zeroCounter = 0, lastZeroCount;
     do {
-        printTable(pboard);
+        printTable(pBoard);
+        lastZeroCount = zeroCounter;
         zeroCounter = 0;
         for (int i = 0; i < BOARD_SIZE; ++i) {
             for (int j = 0; j < BOARD_SIZE; ++j) {
-                if (pboard[i][j][0] == 0) {
+                if (pBoard[i][j][0] == 0) {
                     zeroCounter++;
 
-                    pboard[i][j] = possibilities(pboard, i, j);
+                    pBoard[i][j] = possibilities(pBoard, i, j);
 
-                    int taeller = 0;
+                    int counter = 0;
                     int fundetTal = 0;
                     for (int k = 1; k < MULIGE_INTS; ++k) {
-                        if (pboard[i][j][k] != 0) {
-                            taeller++;
-                            fundetTal = pboard[i][j][k];
+                        if (pBoard[i][j][k] != 0) {
+                            counter++;
+                            fundetTal = pBoard[i][j][k];
                         }
                     }
-                    if (taeller == 1) {
-                        pboard[i][j][0] = fundetTal;
+                    if (counter == 1) {
+                        pBoard[i][j][0] = fundetTal;
                     }
                 }
             }
         }
-    } while (zeroCounter != 0);
-    return pboard;
+    } while ((zeroCounter != 0) && (lastZeroCount != zeroCounter));
+    return pBoard;
 }
 
 
 
-int * possibilities(int*** pboard, int i, int j) {
-    int* posiArray = malloc((sizeof (int)) * MULIGE_INTS);
+int * possibilities(int*** pBoard, int i, int j) {
+    int* posArray = malloc((sizeof (int)) * MULIGE_INTS);
 
     for (int k = 0; k < MULIGE_INTS; ++k) {
-        posiArray[k] = k;
+        posArray[k] = k;
     }
 
     int endValI = ((i + 3) / 3) * 3;
@@ -182,10 +191,10 @@ int * possibilities(int*** pboard, int i, int j) {
 
     for (int k = endValI - 3; k < endValI; ++k) {
         for (int l = endValJ - 3; l < endValJ; ++l) {
-            if (pboard[k][l][0] != 0) {
-                int index = findIndexOf(pboard[k][l][0], posiArray);
+            if (pBoard[k][l][0] != 0) {
+                int index = findIndexOf(pBoard[k][l][0], posArray);
                 if (index > -1) {
-                    posiArray[index] = 0;
+                    posArray[index] = 0;
                 }
             }
         }
@@ -193,32 +202,32 @@ int * possibilities(int*** pboard, int i, int j) {
 
     for (int x = 0; x < BOARD_SIZE; ++x) {
         for (int y = 0; y < BOARD_SIZE; ++y) {
-            if (y == j && pboard[x][y][0] != 0 || x == i && pboard[x][y][0] != 0) {
-                int index = findIndexOf(pboard[x][y][0], posiArray);
+            if (y == j && pBoard[x][y][0] != 0 || x == i && pBoard[x][y][0] != 0) {
+                int index = findIndexOf(pBoard[x][y][0], posArray);
                 if (index > -1) {
-                    posiArray[index] = 0;
+                    posArray[index] = 0;
                 }
             }
         }
     }
-    return posiArray;
+    return posArray;
 }
 
-int findIndexOf(int numb, int *posiArray) {
+int findIndexOf(int numb, int *posArray) {
     for (int i = 0; i < MULIGE_INTS; ++i) {
-        if (posiArray[i] == numb) {
+        if (posArray[i] == numb) {
             return i;
         }
     }
     return -1;
 }
 
-void free3DpArray(int*** pboard) {
+void free3DpArray(int*** pBoard) {
     for (int i = 0; i < BOARD_SIZE; ++i) {
         for (int j = 0; j < BOARD_SIZE; ++j) {
-            free(pboard[i][j]);
+            free(pBoard[i][j]);
         }
-        free(pboard[i]);
+        free(pBoard[i]);
     }
 }
 
